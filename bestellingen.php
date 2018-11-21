4aa8ed361cae6a52b7e7f3459eadf04a6363229c
SylvanLoves <lovessylvan98@gmail.com>
2018-11-21 13:48:40 +0100

    Update bestellingen.php

--- bestellingen.php
+++ bestellingen.php
@@ -1,6 +1,5 @@
 <?php
 include 'dbconfig.php';
-error_reporting(0);
 
 if(isset($_GET['delete_id']))
 {
@@ -13,7 +12,7 @@ if(isset($_GET['delete_id']))
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Bestellingen</title>
+    <title>Video's</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="css/style.css">
@@ -26,8 +25,7 @@ if(isset($_GET['delete_id']))
 <body>
 
 <div class="jumbotron bg-1 text-center">
-    <h1>Seans Surfboards</h1>
-    <p>For all your custom surfboards</p>
+    <h1>Youtube Video's</h1>
 </div>
 
 <div class="container-fluid bg-2">
@@ -42,34 +40,21 @@ if(isset($_GET['delete_id']))
     <tr>
     <th colspan="11"><a href="toevoegen.php">Voeg hier iemand toe</a></th>
     </tr>
-    <th>Bestelnummer</th>
-	<th>Naam</th>
-    <th>Adres</th>
-    <th>Email</th>
-	<th>Telefoon</th>
-	<th>Postcode</th>
-    <th>Afmetingen</th>
-    <th>Datumop</th>
-	<th>Datumaf</th>
-	
-
-    <th colspan="2">Activiteiten</th>
+    <th>Id</th>
+	<th>Artist</th>
+    <th>Song Title</th>
+    <th>PlaybackId</th>
 	<?php
-   $query = "SELECT Bestelnummer , Naam , Adres , Email , Telefoon , Postcode , Afmetingen , Datumop , Datumaf FROM  bestellingen" ;
+   $query = "SELECT ID , Artist , Song Title , PlaybackId" ;
 $result = mysqli_query($conn,$query);
 
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>" . $row["Bestelnummer"]. "</td>
-			  <th> " . $row["Naam"]. "</th>
-			  <th> " . $row["Adres"]. "</th>
-			  <th>" . $row["Email"]. "</th>
-			  <th> " . $row["Telefoon"]. "</th>
-			  <th> " . $row["Postcode"]. "</th>
-			  <th>" . $row["Afmetingen"]. "</th>
-			  <th> " . $row["Datumop"]. "</th>
-			  <th>" . $row["Datumaf"]. "</th>"
+        echo "<tr><td>" . $row["ID"]. "</td>
+			  <th> " . $row["Artist"]. "</th>
+			  <th> " . $row["Song Title"]. "</th>
+			  <th>" . $row["PlaybackId"]. "</th>"
 			  ?>
 			  <td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.jpg" height="20px" width="20px" align="EDIT" /></a></td>
         <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png"  height="20px" width="20px" align="DELETE" /></a></td>
